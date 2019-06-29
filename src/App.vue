161399c5895f72eadc9539d3dd8edb44b5c62579
David Newbury <david.newbury@gmail.com>
2019-06-29 12:52:47 +0200

    Beginning to make this a real boy

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Date Testing Application
+      </h1>
+      <div class="columns">
+        <div class="column is-half">
+          <DateField />
+        </div>
+        <div class="column"><p></p></div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import DateField from "./components/DateField.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    DateField
   }
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
 import App from "./App.vue";
 
+Vue.use(Buefy, { defaultIconPack: "fas" });
 Vue.config.productionTip = false;
 
 new Vue({
